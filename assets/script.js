07b7f821910a855ead155cf816f4958567d299e1
Peter Holcomb <pholcomb91@gmail.com>
2022-09-07 11:24:24 -0700

    beginning the build. all files appropriately organized

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var location= document.getElementById("cityWeather").ariaValueText;
+console.log(location)
+
+/*getLocationWeather()
+    var geoLocation= http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=693570c83db4d8972c67bd010c506b90
+        fetch(geoLocation)
+            .then((response) => response.json())
+            .then((data)=> console.log(data))
+
+    var weather= https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=hourly&appid=693570c83db4d8972c67bd010c506b90;*/
