ab0b4921ffa328000755130050b2a2993e6a26ba
Peter Holcomb <pholcomb91@gmail.com>
2022-09-14 13:57:02 -0700

    feat: buttons working

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,25 @@
 let key = 0;
+let today =moment().format("MMM, Do YYYY");
 
 function getLocationWeather() {
   event.preventDefault();
   var cityEl= document.getElementById("cityWeather");
   var cityValue= cityEl.value.trim();
-  console.log(cityValue);
+  document.getElementById("city").innerHTML=cityValue;
+  document.getElementById("date").innerHTML=today;
   var searchHistory = document.createElement("button");
   searchHistory.innerHTML = cityValue;
-  //for (i=0, )
   searchHistory.id=cityValue;
   document.getElementById("searchHistory").appendChild(searchHistory);
   var ulLength=document.getElementsByTagName("button").length-1;
   console.log(ulLength)
   localStorage.setItem("button-" + key, cityValue);
   key = (key +1) % 10;
-  /*if (cityValue !== "") {
-    localStorage.setItem("City", cityValue);
-  } else {
-    return;
-  }*/
   
 
   var geoApiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityValue + '&limit=1&appid=693570c83db4d8972c67bd010c506b90';
       fetch(geoApiUrl).then(handleGeoLocation);
 }
-    /*fetch(apiUrl)
-      .then(function (response) {
-        if (response.ok) {
-          response.json().then(function (data) {
-            displayWeather(data, );
-          });
-        } else {
-          alert('Error: ' + response.statusText);
-        }
-      })
-      .catch(function (error) {
-        alert('Unable to connect to Open Weather Geolocation');
-      });*/
-
-
-/*getLocationWeather()
-    var geoLocation= http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit=1&appid=693570c83db4d8972c67bd010c506b90
-        fetch(geoLocation)
-            .then((response) => response.json())
-            .then((data)=> console.log(data))
-
-    var weather= https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude=hourly&appid=693570c83db4d8972c67bd010c506b90;*/
 
 function handleGeoLocation(response) {
     if (response.ok) {
@@ -66,14 +40,42 @@ function handleGeoLocationData (data) {
 function weather(response) {
   if (response.ok) {
     response.json().then(function displayWeather(data) {
-      console.log(data.current.temp)
-      console.log(data.current.wind_speed)
-      console.log(data.current.humidity)
+
+      document.getElementById("currentTemp").innerHTML="It is currently " + (data.current.temp)+ " degrees.";
+      document.getElementById("currentWindSpeed").innerHTML="Wind Speed is " + (data.current.wind_speed) + " mph.";
+      document.getElementById("currentHumidity").innerHTML=(data.current.humidity)+"% humidity.";
+      document.getElementById("currentUvi").innerHTML="With a Ultraviolet Index of "+(data.current.uvi);
+      if (data.current.uvi <= 2) {
+        var warning=document.getElementById("currentUvi");
+        warning.classList.add("uviEnjoy");
+      } else if (data.current.uvi > 2 && data.current.uvi < 6) {
+        var warning=document.getElementById("currentUvi");
+        warning.classList.add("uviCaution");
+      } else if (data.current.uvi >= 6 && data.current.uvi < 8) {
+        var warning=document.getElementById("currentUvi");
+        warning.classList.add("uviWarning");
+      } else (data.current.uvi >= 8 && data.current.uvi < 11); {
+        var warning=document.getElementById("currentUvi"); 
+        warning.classList.add("uviAvoid");
+      }
+      //------- above, current --------- below, forecast------
+      
+      for (i=1; i <= 5; i++) {
+        var date=moment.unix(data.daily[i].dt).format("MMM Do, YYYY");
+        var tempDay=(data.daily[i].temp.day);
+        var windSpeed=(data.daily[i].wind_speed);
+        var humidity=(data.daily[i].humidity);
+        document.getElementById("date" + i).innerHTML=date;
+        document.getElementById("tempDay"+ i).innerHTML=tempDay;
+        document.getElementById("windSpeed"+ i).innerHTML=windSpeed;
+        document.getElementById("humidity"+ i).innerHTML=humidity;
+      }
       });
   } else {
       alert('Error: ' + response.statusText);
   }
 }
+
 function init (){
   for ( let i=0; i<= 9; i++) {
     var getSearchHistory = localStorage.getItem("button-"+i);
