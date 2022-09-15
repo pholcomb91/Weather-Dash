94c408048df9eed8003f24df5de11314e76bffb0
Peter Holcomb <pholcomb91@gmail.com>
2022-09-15 09:54:30 -0700

    final: Everything done.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Weather-Dash
+## Weather-Dash
 Search your town or destination and see the weather.
+
+## Credit
+With assistance from Julian: https://github.com/juliandanciu
+
+## Screenshot
+!(./assets/weatherHomePage.png?raw=true "Weather Dash Homepage")
+
+## Links
+https://github.com/pholcomb91/Weather-Dash.git
+https://pholcomb91.github.io/Weather-Dash/
--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 let key = 0;
 let today =moment().format("MMM, Do YYYY");
-
+var reSearch= document.getElementById("searchHistory");
+//Entry
 function getLocationWeather() {
   event.preventDefault();
   var cityEl= document.getElementById("cityWeather");
@@ -30,21 +31,68 @@ function handleGeoLocation(response) {
 }
 
 function handleGeoLocationData (data) {
-    console.log(data[0].lat)
-    console.log(data[0].lon)
-    var weatherApiUrl='https://api.openweathermap.org/data/3.0/onecall?lat=' + (data[0].lat) + '&lon=' + (data[0].lon) + '&exclude=currently,minutely,hourly&units=imperial&appid=693570c83db4d8972c67bd010c506b90';
-    fetch(weatherApiUrl).then(weather); 
+  var weatherApiUrl='https://api.openweathermap.org/data/3.0/onecall?lat=' + (data[0].lat) + '&lon=' + (data[0].lon) + '&exclude=currently,minutely,hourly&units=imperial&appid=693570c83db4d8972c67bd010c506b90';
+  fetch(weatherApiUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        alert('Error: ' + response.statusText);
+        return;
+      }
+      return response.json();
+    })
+    .then(function (data) {
+        var cityEl= document.getElementById("cityWeather");
+        var cityValue= cityEl.value.trim();
+        city=JSON.stringify(data)
+        localStorage.setItem(cityValue, city)
+        displayWeather(data);
+    })
+    .catch(function (error) {
+      console.error(error)
+    })
+  }
+    /*if (response.ok) {
+      response.json().then(
+      displayWeather(data)
+      ).then(function(city) {
+      city=JSON.stringify(city)
+      localStorage.setItem(cityValue, city)
+      }
+      )
+
+      //todo: stringify data. save inside local store.  
+    } /*else {
+     
+    }*/
+
+function savedWeather (city) {
+  var savedCity=localStorage.getItem(city)
+  savedCity=JSON.parse(savedCity)
+  document.getElementById("city").innerHTML=city;
+  document.getElementById("date").innerHTML=today;
+  displayWeather (savedCity)
+  //todo: retrieve local data.
+
 }
 
+function init (){
+  for ( let i=0; i<= 9; i++) {
+    var getSearchHistory = localStorage.getItem("button-"+i);
+        if (getSearchHistory !== null) {
+          var searchHistory = document.createElement("button");
+          searchHistory.innerHTML = getSearchHistory;
+          document.getElementById("searchHistory").appendChild(searchHistory);
+        } 
+  }
+}
 
-function weather(response) {
-  if (response.ok) {
-    response.json().then(function displayWeather(data) {
-
+function displayWeather (data) {
+      //document.getElementById("city").innerHTML=cityValue;
+      //document.getElementById("date").innerHTML=today;
       document.getElementById("currentTemp").innerHTML="It is currently " + (data.current.temp)+ " degrees.";
       document.getElementById("currentWindSpeed").innerHTML="Wind Speed is " + (data.current.wind_speed) + " mph.";
       document.getElementById("currentHumidity").innerHTML=(data.current.humidity)+"% humidity.";
-      document.getElementById("currentUvi").innerHTML="With a Ultraviolet Index of "+(data.current.uvi);
+      document.getElementById("currentUvi").innerHTML="With an Ultraviolet Index of "+(data.current.uvi);
       if (data.current.uvi <= 2) {
         var warning=document.getElementById("currentUvi");
         warning.classList.add("uviEnjoy");
@@ -58,6 +106,7 @@ function weather(response) {
         var warning=document.getElementById("currentUvi"); 
         warning.classList.add("uviAvoid");
       }
+      
       //------- above, current --------- below, forecast------
       
       for (i=1; i <= 5; i++) {
@@ -70,20 +119,11 @@ function weather(response) {
         document.getElementById("windSpeed"+ i).innerHTML="Wind Speed " +windSpeed;
         document.getElementById("humidity"+ i).innerHTML="Humidity " +humidity;
       }
-      });
-  } else {
-      alert('Error: ' + response.statusText);
-  }
 }
 
-function init (){
-  for ( let i=0; i<= 9; i++) {
-    var getSearchHistory = localStorage.getItem("button-"+i);
-        if (getSearchHistory !== null) {
-          var searchHistory = document.createElement("button");
-          searchHistory.innerHTML = getSearchHistory;
-          document.getElementById("searchHistory").appendChild(searchHistory);
-        } 
-  }
-}
-init ()
+reSearch.addEventListener("click", function fetchLocal (event) {
+  var city=event.target.innerHTML
+  savedWeather (city) 
+})
+
+init ();
